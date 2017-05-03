d2c6ccd932f3ec528f192ca6aeaf60fe48c4da6f
Fernando-Freire <fernando.scattone@usp.br>
2017-05-03 15:28:38 -0300

    update, search and destroy capabilities
    Signed-off-by: Frederico Lage <fredericolf@gmail.com>
    Signed-off-by: Fernando-Freire <fernando.scattone@usp.br>

--- app/controllers/capabilities_controller.rb
+++ app/controllers/capabilities_controller.rb
@@ -4,32 +4,45 @@ class CapabilitiesController < ApplicationController
 
   # GET /capabilities
   def index
-
-    capabilities = Capability.all
-
-
-
-    render json: { capabilities: capabilities  } ,status: 200
+    type = params[:capability_type]
+    if(type == nil)
+      capabilities = Capability.all
+    elsif(type == "sensor")
+      capabilities = Capability.where(function: 0)
+    elsif(type == "actuator")
+      capabilities = Capability.where(function: 1)
+    elsif(type == "information")
+      capabilities = Capability.where(function: 2)
+    else
+      capabilities = "could not find by capability type"
+    end
+    if (capabilities.is_a? String)
+      render json: { error: capabilities  } ,status: 400
+    else
+      render json: { capabilities: capabilities  } ,status: 200
+    end
   end
 
 
   #POST /capabilities
   def create
-    stat =201
-    capability_type = params[:capability_type]
-    puts "*"* 10000
-    puts params.inspect
-    if capability_type == "sensor"
-      capability = Capability.create_sensor(create_params)
-      puts capability
-    elsif capability_type == "actuator"
-      capability = Capability.create_actuator(create_params)
-      puts capability
-    else
-   	  stat = 400
-   	  capability = { erro:  "invalid capability_type  #{capability_type}"  }
+    begin
+      capability_type = params[:capability_type]
+      puts params.inspect
+      if capability_type == "sensor"
+        capability = Capability.create_sensor(create_params)
+        puts capability
+      elsif capability_type == "actuator"
+        capability = Capability.create_actuator(create_params)
+        puts capability
+      elsif capability_type == "information"
+        capability = Capability.create_information(create_params)
+        puts capability
+      end
+    rescue Exception => e
+      render json: { erro: e  }, status: 400
     end
-    render json: { capability: capability } , status: stat
+    render json: { capability: capability } , status: 201
   end 
 
 
@@ -37,17 +50,34 @@ class CapabilitiesController < ApplicationController
 
 
   #PATCH /capabilities/:name
+  #PUT /capabilities/:name
   def update
-    status =201
-    capability_name, capability_description = update_params
-    capability = Capability.find_by_name capability_name
-    capability.description = capability_description
-
-
     
-
-    render json: { capability: capability}, status = status
+    begin
+      
+      capability = Capability.find_by_name params[:name]
+      capability.update!(update_params)
+      render json: { capability: capability}, status: 201
+    rescue Exception => e
+      puts e.message
+      render json: {error: e }, status: 400
+    end
   end
+
+
+  #DELETE /capabilities/:id
+  def destroy
+    capability = Capability.find_by_id params[:id]
+    begin
+      capability.delete
+    rescue
+      render json: {error: "No capability found"}, status: 400
+    end
+  end 
+
+
+
+
 
 
   private
@@ -55,6 +85,6 @@ class CapabilitiesController < ApplicationController
 	    params.permit(:name, :description)
     end
     def update_params
-      params.permit(:name, :description)
+      params.permit(:name, :description )
     end
 end
